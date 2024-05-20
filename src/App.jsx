35df6fb59d478385ddb3d8c5f048f0c8af4cf97f
Steve Tyler <steve.tyler75@gmail.com>
2024-05-20 13:36:45 +0100

    section 184

--- src/App.jsx
+++ src/App.jsx
@@ -65,7 +65,7 @@ function App() {
 
   return (
     <>
-      <Modal open={modalIsOpen}>
+      <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
           onConfirm={handleRemovePlace}
